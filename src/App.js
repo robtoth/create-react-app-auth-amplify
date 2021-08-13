1130ca699a90f241a25e5f500029121b01d346c4
Rob Toth <robtoth@tothtechnology.com>
2021-08-13 03:09:11 -0400

    I get the user id for the uploaded sharable link.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify, { Storage } from 'aws-amplify';
+import Amplify, { Storage, Auth } from 'aws-amplify';
 
 import aws_exports from './aws-exports';
 console.log(aws_exports)
@@ -11,16 +11,19 @@ Amplify.configure(aws_exports);
 
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
 }
 
+
 class App extends Component {
   state = {
+    email: "",
     imageName: "",
     imageFile: "",
-    response: ""
+    response: "",
+    sharable_link_uri: 'https://meddyhealth.co'
   };
 
   uploadImage = () => {
@@ -35,14 +38,31 @@ class App extends Component {
                 })
       .then(result => {
         this.upload = null;
-        this.setState({ response: "Success uploading file!" });
+        this.setState({ response: "Success uploading file! It may take a few minutes until that file is processed." });
       })
       .catch(err => {
         this.setState({ response: `Cannot upload file: ${err}` });
       });
   };
 
+  updateSharableLink = () => {
+    
+    Auth.currentUserCredentials().then( credentials => {
+      
+      const identityId = credentials.identityId
+      //console.log(identityId);
+      
+      Storage.get(identityId + '/test_upload.zip', {bucket: 'meddy-sharing'}).then( result => {
+        console.log(result);
+        this.setState({sharable_link_uri: result})
+      }).catch( error => {
+        console.error(error);
+      });
+    });
+  }
+
   render() {
+    
     return (
           
       <div className="App">
@@ -84,15 +104,15 @@ class App extends Component {
         <button onClick={this.uploadImage}> Upload File </button>
 
         {!!this.state.response && <div>{this.state.response}</div>}
-        <p class='info'>Upload progress will be shown here.</p>
+        <p className='info'>Upload progress will be shown here.</p>
         
-        <div class='sharable'>
+        <div className='sharable'>
           <h1>Sharable Link:</h1>
-          <i>
-          <p class='info'>Note: Refresh this page to regenerate the link.</p>
-          <p class='info'>This link expires in 15 minutes.</p>
-          </i>
-          <h3><a href='www.google.com'>www.google.com</a></h3>
+          <button onClick={this.updateSharableLink}>Generate Secure Link</button>
+          
+          <p className='info'>Note: This link expires in 15 minutes.</p>
+          
+          <h3><a href={this.state.sharable_link_uri} target='blank'>{this.state.sharable_link_uri}</a></h3>
           <p>For help, please email <a href='mailto:support@meddyhealth.co'>support@meddyhealth.co</a></p>
         </div>
         
